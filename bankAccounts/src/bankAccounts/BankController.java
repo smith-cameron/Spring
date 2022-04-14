6f2c072a9be3fee43c400e6bb469021b7a3fdabd
theauspiciouscammy <theauspiciouscammy@gmail.com>
2022-04-14 07:12:22 -0700

    update

---
+++ bankAccounts/src/bankAccounts/BankController.java
@@ -0,0 +1,29 @@
+package bankAccounts;
+
+public class BankController {
+
+	public static void main(String[] args) {
+		BankAccount ba = new BankAccount();
+        System.out.println("New Acc No. - " + ba.getAccountNumber());
+		ba.depositMoney(100.49, "checking");
+		ba.depositMoney(3.00, "savings");
+		ba.displayAccountBalance();
+
+		System.out.println(BankAccount.totalHoldings);
+
+		ba.withdrawMoney(50, "checking");
+		ba.withdrawMoney(50, "savings");
+		ba.displayAccountBalance();
+
+		System.out.println(BankAccount.totalHoldings);
+
+        BankAccount ba2 = new BankAccount();
+        System.out.println("New Acc No. - " + ba2.getAccountNumber());
+		ba2.depositMoney(3, "checking");
+		ba2.depositMoney(99, "savings");
+		ba2.displayAccountBalance();
+        ba2.withdrawMoney(50, "checking");
+
+	}
+
+}
