3770a13c1153e66de7a272ef4c52dc07867096de
theauspiciouscammy <theauspiciouscammy@gmail.com>
2022-05-13 16:05:45 -0700

    update

--- saveTravels/src/main/java/com/cameron/saveTravels/repositories/ExpenseRepo.java
+++ saveTravels/src/main/java/com/cameron/saveTravels/repositories/ExpenseRepo.java
@@ -3,9 +3,10 @@ package com.cameron.saveTravels.repositories;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.cameron.saveTravels.models.Expense;
-
+@Repository
 public interface ExpenseRepo extends CrudRepository<Expense, Long> {
 	List<Expense> findAll();
 
