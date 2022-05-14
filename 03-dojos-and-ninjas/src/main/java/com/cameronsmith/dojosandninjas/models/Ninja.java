fbecc30bbc0415a8cfae337b127639aa43632275
theauspiciouscammy <theauspiciouscammy@gmail.com>
2022-05-14 08:31:25 -0700

    update

--- 03-dojos-and-ninjas/src/main/java/com/cameronsmith/dojosandninjas/models/Ninja.java
+++ 03-dojos-and-ninjas/src/main/java/com/cameronsmith/dojosandninjas/models/Ninja.java
@@ -20,24 +20,27 @@ import javax.validation.constraints.Past;
 import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
-@Table(name="ninjas")
+@Table(name = "ninjas")
 public class Ninja {
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	@Column(updatable=false)
+	@Column(updatable = false)
 	@DateTimeFormat(pattern = "MM/dd/yyyy HH:mm:ss")
 	private Date createdAt;
 	@DateTimeFormat(pattern = "MM/dd/yyyy HH:mm:ss")
 	private Date updatedAt;
+
 	@PrePersist
-	protected void onCreate(){
+	protected void onCreate() {
 		this.createdAt = new Date();
 	}
+
 	@PreUpdate
-	protected void onUpdate(){
+	protected void onUpdate() {
 		this.updatedAt = new Date();
 	}
+
 	@NotBlank
 	private String firstName;
 	@NotBlank
@@ -46,59 +49,72 @@ public class Ninja {
 	@Past(message = "Must be born to be a student")
 	private int age;
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name="dojo_id")
-    private Dojo dojo;
-    
-    public Ninja() {
-        
-    }
+	@JoinColumn(name = "dojo_id")
+	private Dojo dojo;
+
+	public Ninja() {
+
+	}
 //	public Ninja(String firstname, String lastname, int age) {
 //		this.firstName=firstname;
 //		this.lastName=lastname;
 //		this.age=age;
 //	}
-	
+
 	public Long getId() {
 		return this.id;
 	}
+
 	public Date getCreatedAt() {
 		return this.createdAt;
 	}
+
 	public Date getUpdatedAt() {
 		return this.updatedAt;
 	}
+
 	public String getFirstName() {
 		return this.firstName;
 	}
+
 	public String getLastName() {
 		return this.lastName;
 	}
+
 	public int getAge() {
 		return this.age;
 	}
+
 	public Dojo getDojo() {
 		return this.dojo;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public void setCreatedAt(Date createdAt) {
 		this.createdAt = createdAt;
 	}
+
 	public void setUpdatedAt(Date updatedAt) {
 		this.updatedAt = updatedAt;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
+
 	public void setDojo(Dojo dojo) {
 		this.dojo = dojo;
 	}
-    
+
 }
