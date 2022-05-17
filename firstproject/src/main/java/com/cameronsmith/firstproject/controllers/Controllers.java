1e370fb5502e941d9ba5d2868c11938f9ffa717d
theauspiciouscammy <theauspiciouscammy@gmail.com>
2022-05-17 15:54:22 -0700

    update

--- firstproject/src/main/java/com/cameronsmith/firstproject/controllers/Controllers.java
+++ firstproject/src/main/java/com/cameronsmith/firstproject/controllers/Controllers.java
@@ -7,26 +7,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @Controller
 public class Controllers {
-	@RequestMapping(value="/", method=RequestMethod.GET)
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index() {
-        return "index.jsp";
+		return "index.jsp";
 	}
-	
-	@RequestMapping(value="/addJoke", method=RequestMethod.POST)
-	public String addJoke(@RequestParam("name")String nameInput, @RequestParam("joke")String jokeInput, Model viewModel) {
+
+	@RequestMapping(value = "/addJoke", method = RequestMethod.POST)
+	public String addJoke(@RequestParam("name") String nameInput, @RequestParam("joke") String jokeInput,
+			Model viewModel) {
 		viewModel.addAttribute("name", nameInput);
 		viewModel.addAttribute("joke", jokeInput);
 		return "showJoke.jsp";
-		
+
 	}
-	
-	@RequestMapping(value="/colors/{color}", method=RequestMethod.GET)
-	public String coolColors(@PathVariable("color")String color, Model viewModel) {
+
+	@RequestMapping(value = "/colors/{color}", method = RequestMethod.GET)
+	public String coolColors(@PathVariable("color") String color, Model viewModel) {
 		viewModel.addAttribute("color", color);
-        return "color.jsp";
+		return "color.jsp";
 	}
-	
+
 }
