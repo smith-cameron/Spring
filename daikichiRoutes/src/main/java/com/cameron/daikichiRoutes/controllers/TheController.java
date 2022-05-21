516518691738e93e3a668f196bf4246f2f9d7e01
theauspiciouscammy <theauspiciouscammy@gmail.com>
2022-05-21 08:16:04 -0700

    update

--- daikichiRoutes/src/main/java/com/cameron/daikichiRoutes/controllers/TheController.java
+++ daikichiRoutes/src/main/java/com/cameron/daikichiRoutes/controllers/TheController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TheController {
 	@RequestMapping("/")
-	public String indexRoute(){
-		
+	public String indexRoute() {
+
 		return "";
 	}
 }
