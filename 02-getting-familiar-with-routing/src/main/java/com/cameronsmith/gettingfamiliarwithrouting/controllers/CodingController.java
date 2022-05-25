8da7854df655d344b83feaeefadb90d5dd829579
theauspiciouscammy <theauspiciouscammy@gmail.com>
2022-05-24 17:05:17 -0700

    update

--- 02-getting-familiar-with-routing/src/main/java/com/cameronsmith/gettingfamiliarwithrouting/controllers/CodingController.java
+++ 02-getting-familiar-with-routing/src/main/java/com/cameronsmith/gettingfamiliarwithrouting/controllers/CodingController.java
@@ -1,10 +1,9 @@
 package com.cameronsmith.gettingfamiliarwithrouting.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-@Controller
+
 @RestController
 public class CodingController {
 	@RequestMapping(value="/coding", method=RequestMethod.GET)
