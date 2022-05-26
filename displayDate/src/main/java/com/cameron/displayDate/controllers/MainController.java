5342c53a1350285a5e8eee60b66c28b06af8fc44
theauspiciouscammy <theauspiciouscammy@gmail.com>
2022-05-25 20:09:01 -0700

    update

--- displayDate/src/main/java/com/cameron/displayDate/controllers/MainController.java
+++ displayDate/src/main/java/com/cameron/displayDate/controllers/MainController.java
@@ -10,23 +10,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 	Date today = new Date();
-	
+
 	@GetMapping("/")
 	public String index(Model model) {
 		return "index.jsp";
 	}
+
 	@GetMapping("/date")
 	public String date(Model model) {
 		String pattern = "EEEEE, 'the' dd 'of' MMMMM, yyyy";
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
 		model.addAttribute("todaysDate", simpleDateFormat.format(today));
-		
+
 		return "date.jsp";
 	}
+
 	@GetMapping("/time")
 	public String time(Model model) {
 		model.addAttribute("todaysTime", today);
-		
+
 		return "time.jsp";
 	}
 }
